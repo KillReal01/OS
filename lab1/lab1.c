6d412ab7553f886fa594fc2566d096c105e339d5
killreal killreal <kirill.bereza.02@mail.ru>
2022-02-20 19:14:09 +0300

    added

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -8,28 +8,33 @@ typedef struct{
 } targs;
 
 void *proc1(void *arg){
+    printf("\nПоток 1 начал работу\n");
     targs* args = (targs*) arg;
     while (args->flag == 0){
+        sleep(1);
         putchar(args->sym);
         fflush(stdout);
-        sleep(1);
     }
+    printf("\nПоток 1 завершил работу\n");
     pthread_exit((void*)1);
 }
 
 void *proc2(void *arg){
+    printf("\nПоток 2 начал работу\n");
     targs* args = (targs*) arg;
     while (args->flag == 0){
+        sleep(1);
         putchar(args->sym);
         fflush(stdout);
-        sleep(1);
     }
+    printf("\nПоток 2 завершил работу\n");
     pthread_exit((void*)2);
 }
 
 
  int main()
  {
+     printf("Программа начала работу\n");
      pthread_t id1, id2;
      targs arg1, arg2;
      arg1.flag = 0;
@@ -41,13 +46,16 @@ void *proc2(void *arg){
 
      pthread_create(&id1, NULL, proc1, &arg1);
      pthread_create(&id2, NULL, proc2, &arg2);
+     printf("\nПрограмма ждет нажатия клавиши\n");
      getchar();
+     printf("\nКлавиша нажата\n");
      arg1.flag = 1;
      arg2.flag = 1;
      pthread_join(id1, (void**)&exitcode1);
      pthread_join(id2, (void**)&exitcode2);
 
-     printf("exitcode1 = %p\n", exitcode1);
+     printf("\nexitcode1 = %p\n", exitcode1);
      printf("exitcode2 = %p\n", exitcode2);
+     printf("\nПрограмма завершила работу\n");
      return 0;
  }
