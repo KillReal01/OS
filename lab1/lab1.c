ab92ab72a54472e48db027a2e6390c562ec233d0
killreal killreal <kirill.bereza.02@mail.ru>
2022-02-22 18:03:41 +0300

    added

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -39,25 +39,34 @@ void *proc2(void *arg){
      printf("Программа начала работу\n");
      pthread_t id1, id2;
      targs arg1, arg2;
-     arg1.flag = 0;
-     arg2.flag = 0;
-     arg1.sym = '1';
-     arg2.sym = '2';
-
+     cpu_set_t cpu1, cpu2;
      int *exitcode1, *exitcode2;
+     
+     arg1 = {0, '1'};
+     arg2 = {0, '2'};
 
+     CPU_ZERO(&cpu1);
+     CPU_SET(1, &cpu1);
      pthread_create(&id1, NULL, proc1, &arg1);
+     pthread_setaffinity_np(id1, sizeof(cpu_set_t), &cpu1);
+
+     CPU_ZERO(&cpu2);
+     CPU_SET(2, &cpu2);
      pthread_create(&id2, NULL, proc2, &arg2);
+     pthread_setaffinity_np(id2, sizeof(cpu_set_t), &cpu2);
+     
      printf("\nПрограмма ждет нажатия клавиши\n");
      getchar();
      printf("\nКлавиша нажата\n");
      arg1.flag = 1;
      arg2.flag = 1;
+     
      pthread_join(id1, (void**)&exitcode1);
      pthread_join(id2, (void**)&exitcode2);
-
+     
      printf("\nexitcode1 = %p\n", exitcode1);
      printf("exitcode2 = %p\n", exitcode2);
      printf("\nПрограмма завершила работу\n");
+
      return 0;
  }
